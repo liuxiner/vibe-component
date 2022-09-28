722cda3ccc7a807c4cc0349eb7791043c1956630
liuxiner <1223495869@qq.com>
2022-09-28 23:53:17 +0800

    UPDATE: react scaffold

--- my/src/App.js
+++ my/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { ScrollFeatures } from './scroll-features';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ScrollFeatures />
     </div>
   );
 }
---
+++ my/src/scroll-features/index.js
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+import React from "react";
+
+export default function ScrollFeatures() {
+  return (
+    <section className="scroll-features">
+      <div className="contanier">
+
+      </div>
+    </section>
+  );
+}
